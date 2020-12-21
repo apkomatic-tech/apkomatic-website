21057e88e13a720af60a2be0ae619a131ea152a2
Konstantin M <humbl3dev@gmail.com>
2020-12-21 11:34:56 -0800

    adds splash banner containter

---
+++ src/components/SplashBanner.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+const StyledBanner = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 30vh;
+  position: relative;
+  z-index: 1;
+  background-color: var(--grey);
+  text-align: center;
+  overflow: hidden;
+`;
+const StyledBannerContainer = styled(motion.div)`
+  max-width: 60rem;
+  margin: auto;
+  padding: 4rem 2rem;
+`;
+const StyledTitle = styled(motion.h1)`
+  margin-bottom: 2.5rem;
+  position: relative;
+  z-index: 1;
+  padding: 2rem;
+  border: 3px solid var(--darkColor);
+  box-shadow: 10px 10px 0 rgba(0, 0, 0, 0.4);
+  font-size: 2.6rem;
+  letter-spacing: 0.06em;
+  text-transform: uppercase;
+`;
+
+const SplashBanner = ({ title, message }) => {
+  return (
+    <StyledBanner>
+      <StyledBannerContainer
+        transition={{
+          delay: 0.1,
+        }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+      >
+        <StyledTitle>{title}</StyledTitle>
+        {message && <p>{message}</p>}
+      </StyledBannerContainer>
+    </StyledBanner>
+  );
+};
+
+SplashBanner.propTypes = {
+  title: PropTypes.string.isRequired,
+  message: PropTypes.string,
+};
+SplashBanner.propTypes = {
+  message: '',
+};
+
+export default SplashBanner;
