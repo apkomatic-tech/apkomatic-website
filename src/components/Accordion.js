7c8745f111365b27ca9bc50ff83cc9b107e97ce0
Konstantin M <humbl3dev@gmail.com>
2020-12-21 17:57:04 -0800

    accordion component

---
+++ src/components/Accordion.js
@@ -0,0 +1,129 @@
+import { motion } from 'framer-motion';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { HiOutlineChevronRight } from 'react-icons/hi';
+
+// styled components
+const StyledAccordion = styled.div`
+  border-bottom: 1px solid rgba(0, 0, 0, 0.15);
+  box-sizing: border-box;
+  * {
+    box-sizing: inherit;
+  }
+`;
+const StyledAccordionHeading = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  cursor: pointer;
+  padding: 1.5rem;
+  background: transparent;
+  border-radius: 5px;
+  width: 100%;
+  appearance: none;
+  border: 0;
+  text-align: left;
+`;
+const StyledAccordionToggle = styled(motion.span)`
+  display: block;
+  margin-left: 10px;
+  transform: rotate(90deg);
+  font-size: 1.3rem;
+`;
+const StyledAccordionContent = styled(motion.div)`
+  overflow: hidden;
+  color: rgba(#111, 0.7);
+  background: var(--grey);
+  height: 0;
+  .content {
+    padding: 2rem;
+  }
+`;
+
+// hooks
+function useAccordion(items = []) {
+  const [accItems, setAccItems] = useState(items);
+
+  const toggleVisibility = id => {
+    const index = accItems.findIndex(item => item.id === id);
+    if (index > -1) {
+      const updatedArrayOfAccItems = [
+        ...accItems.slice(0, index),
+        {
+          ...accItems[index],
+          collapsed: !accItems[index].collapsed,
+        },
+        ...accItems.slice(index + 1),
+      ];
+      setAccItems(updatedArrayOfAccItems);
+    }
+  };
+
+  return {
+    accItems,
+    toggleVisibility,
+  };
+}
+
+const Accordion = ({ items, ...rest }) => {
+  const { accItems, toggleVisibility } = useAccordion(items);
+
+  // id, heading, content, collapsed
+  return (
+    <div {...rest}>
+      {accItems.map(({ id, heading, content, collapsed }) => {
+        return (
+          <StyledAccordion key={id}>
+            <StyledAccordionHeading
+              role="button"
+              tabIndex={0}
+              onClick={() => toggleVisibility(id)}
+              onKeyPress={event => {
+                const { key } = event;
+                if (key === 'Enter') {
+                  toggleVisibility(id);
+                }
+              }}
+            >
+              <span>{heading}</span>
+              <StyledAccordionToggle
+                transition={{
+                  easings: 'linear',
+                  duration: 0.2,
+                }}
+                initial={{
+                  rotate: '90deg',
+                }}
+                animate={{
+                  rotate: collapsed ? '90deg' : '-90deg',
+                }}
+              >
+                <HiOutlineChevronRight />
+              </StyledAccordionToggle>
+            </StyledAccordionHeading>
+            <StyledAccordionContent
+              aria-expanded={!collapsed}
+              initial={false}
+              transition={{
+                easings: 'linear',
+                duration: 0.2,
+              }}
+              animate={{
+                height: collapsed ? 0 : 'auto',
+              }}
+            >
+              <div className="content">{content}</div>
+            </StyledAccordionContent>
+          </StyledAccordion>
+        );
+      })}
+    </div>
+  );
+};
+
+Accordion.propTypes = {
+  items: PropTypes.array.isRequired,
+};
+
+export default Accordion;
