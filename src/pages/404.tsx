941f2ae1656787148938fa8f23b492e083c641ac
Konstantin M <humbl3dev@gmail.com>
2021-01-19 11:50:38 -0800

    fix and update styling for 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
+import styled from 'styled-components';
+import { MdSentimentVeryDissatisfied } from 'react-icons/md';
 
 import SEO from '../components/Seo';
+import Wrapper from '../components/Wrapper';
+
+const Styled404Container = styled.div`
+  text-align: center;
+  h1 {
+    margin-top: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    svg {
+      margin-right: 0.5rem;
+      transform: scale(1.1);
+    }
+  }
+`;
 
 const NotFoundPage = () => (
   <div id="four-oh-four-page">
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
+    <Wrapper
+      style={{
+        marginTop: '7rem',
+        marginBottom: '7rem',
+        maxWidth: '45rem',
+      }}
+    >
+      <Styled404Container>
+        <h1>
+          <MdSentimentVeryDissatisfied /> Page not Found
+        </h1>
+        <p>
+          Oops, we can't fine the page you are looking forward. Try going back
+          to the previous page.
+        </p>
+      </Styled404Container>
+    </Wrapper>
   </div>
 );
 
